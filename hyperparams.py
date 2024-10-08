976c57e87f3e044794a1f482c481ec77bcced1b7
PrasannaKasar <154019744+PrasannaKasar@users.noreply.github.com>
2024-10-08 22:18:19 +0530

    Update hyperparams.py

--- hyperparams.py
+++ hyperparams.py
@@ -23,14 +23,14 @@ n_iter = 60
 # power = 1.5
 outputs_per_step = 1
 
-epochs = 10000
+epochs = 200
 lr = 0.001
 save_step = 2000
 image_step = 500
-batch_size = 16
+batch_size = 32
 
 cleaners='english_cleaners'
 
-data_path = '/content/LJSpeech-1.1'
+data_path = '/kaggle/input/ljspeech-1-1-with-mel-and-mag-of-each-audio'
 checkpoint_path = './checkpoint'
 sample_path = './samples'
