b005cb47584759574764609753343679731f7c45
PrasannaKasar <154019744+PrasannaKasar@users.noreply.github.com>
2024-10-18 00:22:13 +0530

    Update hifigan.py

--- hifigan.py
+++ hifigan.py
@@ -24,11 +24,11 @@ def load_checkpoint(checkpoint_path):
 
 def synthesis(text, args):
     MODEL = Model().cuda()
-    MODEL_post = ModelPostNet().cuda()
+    # MODEL_post = ModelPostNet().cuda()
 
     # Load checkpoints using the paths provided via command line arguments
     MODEL.load_state_dict(load_checkpoint(args.transformer_checkpoint))
-    MODEL_post.load_state_dict(load_checkpoint(args.postnet_checkpoint))
+    # MODEL_post.load_state_dict(load_checkpoint(args.postnet_checkpoint))
 
     # Prepare input text
     text = np.asarray(text_to_sequence(text, [hp.cleaners]))
@@ -38,23 +38,24 @@ def synthesis(text, args):
     pos_text = torch.arange(1, text.size(1) + 1).unsqueeze(0).cuda()  # Push to GPU
 
     MODEL.eval()
-    MODEL_post.eval()
+    # MODEL_post.eval()
 
     pbar = tqdm(range(args.max_len))  # For progress bar
 
     with torch.no_grad():
         for i in pbar:
             pos_mel = torch.arange(1, mel_input.size(1) + 1).unsqueeze(0).cuda()
-            mel_pred, postnet_pred, _, _, _, _ = MODEL.forward(text, mel_input, pos_text, pos_mel)
+            mel_pred, postnet_pred, _, stop_pred, _, _ = MODEL.forward(text, mel_input, pos_text, pos_mel)
             mel_input = torch.cat([mel_input, mel_pred[:, -1:, :]], dim=1)
+    print(torch.max(torch.sigmoid(stop_pred)))
+        # mag_pred = MODEL_post.forward(postnet_pred)
 
-        mag_pred = MODEL_post.forward(postnet_pred)
-
-    print("Shape of mag_pred:", mag_pred.shape)  # Add this line for debugging
+    # print("Shape of mag_pred:", mag_pred.shape)  # Add this line for debugging
     # Generate waveform using HiFi-GAN
-    wav = generate_audio_with_hifigan(mag_pred)
+    # wav = generate_audio_with_hifigan(mag_pred)
     # Write output WAV file
-    write(hp.sample_path + "/test.wav", hp.sr, wav)
+    torch.save(postnet_pred, '5k_postnet_mel.pt')
+    # write(hp.sample_path + "/test.wav", hp.sr, wav)
 
 def convert_to_mel_spectrogram(mag_pred):
     # Define the mel filter parameters
@@ -103,7 +104,7 @@ def generate_audio_with_hifigan(mag_pred):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--transformer_checkpoint', type=str, help='Path to the transformer checkpoint file', required=True)
-    parser.add_argument('--postnet_checkpoint', type=str, help='Path to the postnet checkpoint file', required=True)
+    # parser.add_argument('--postnet_checkpoint', type=str, help='Path to the postnet checkpoint file', required=True)
     parser.add_argument('--max_len', type=int, help='Maximum length of the generated mel-spectrogram', default=400)
     parser.add_argument('--text', type=str, help='Text input to synthesize into speech', default="Hello! Good Morning")
 
